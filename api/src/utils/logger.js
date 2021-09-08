9e96bb455a5370a590c0468f4b0b6b0a5dc290c4
Ian King <ian.antking@gmail.com>
2021-09-08 23:19:26 +0100

    add log level to log string

--- api/src/utils/logger.js
+++ api/src/utils/logger.js
@@ -1,16 +1,22 @@
 /* eslint-disable no-console */
 const { format } = require('date-fns');
 
-const formatMessage = (message) => `[${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}]: ${message}`;
+const levels = {
+  info: 'INFO',
+  warn: 'WARN',
+  error: 'ERROR',
+};
+
+const formatMessage = (message, level) => `[${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}] ${levels[level]}: ${message}`;
 
 exports.log = (message) => {
-  console.log(formatMessage(message));
+  console.log(formatMessage(message, 'info'));
 };
 
 exports.warn = (message) => {
-  console.warn(formatMessage(message));
+  console.warn(formatMessage(message, 'warn'));
 };
 
 exports.error = (message) => {
-  console.error(formatMessage(message));
+  console.error(formatMessage(message, 'error'));
 };
