4d0f6d6ca8ca3d53f258ed5cdd8eb999030bfa69
Ian King <ian.antking@gmail.com>
2021-09-08 22:57:38 +0100

    add basic express app

---
+++ api/src/utils/logger.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+exports.log = (message) => {
+  console.log(message);
+};
+
+exports.warn = (message) => {
+  console.warn(message);
+};
+
+exports.error = (message) => {
+  console.error(message);
+};
