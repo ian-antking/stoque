008f5c51f6c808dd7f4464e3286a5cf551fbdbfa
Ian King <ian.antking@gmail.com>
2021-09-08 23:46:47 +0100

    add helmet and cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const { logging } = require('./middleware/logging');
 const userRouter = require('./routes/user');
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(logging);
 
 app.use('/user', userRouter);
