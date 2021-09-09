114ad14a68bdbc5fe36abcaac68f1e02b2bd5b4c
Ian King <ian.antking@gmail.com>
2021-09-09 15:25:08 +0100

    add basic barcode lookup and printing

--- barcode-scanner/barcode-scanner/__main__.py
+++ barcode-scanner/barcode-scanner/__main__.py
@@ -1,4 +1,22 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+BASE_URL = 'https://api.barcodelookup.com/v3/products?barcode={}&formatted=y&key={}'
+
+def generate_url(barcode, api_key):
+  return BASE_URL.format(barcode, api_key)
+
 if __name__ == "__main__":
+  load_dotenv()
+  api_key = os.environ.get('API_KEY')
+  print(api_key)
   while True:
     barcode = input("> ")
-    print(barcode)
+    search_url = generate_url(barcode, api_key)
+    response = requests.get(search_url)
+  
+    if response.ok:
+      print(response.json()['products'][0])
+    else:
+      print("product not found")
